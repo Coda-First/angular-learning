5d6ae113ee35aea40e64450a03979965bff73e0f
KilianOlry <kilian.olry@gmail.com>
2024-04-09 14:16:53 +0200

    feat(nav): add navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'learning-angular';
+  title = 'Benoît Parmentier';
+  item = 0;
 }
