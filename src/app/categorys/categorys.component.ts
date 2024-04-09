0ddb258865867aae8e09cfee565837870408a63d
KilianOlry <kilian.olry@gmail.com>
2024-04-09 21:28:38 +0200

    feat(categorys.html / categorys.ts): add click event

--- src/app/categorys/categorys.component.ts
+++ src/app/categorys/categorys.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-categorys',
   templateUrl: './categorys.component.html',
-  styleUrl: './categorys.component.css'
+  styleUrl: './categorys.component.css',
 })
-export class CategorysComponent {
+export class CategorysComponent implements OnInit {
+  public count: number = 0;
+  constructor() {}
 
+  public increment() {
+    this.count += 1;
+  }
+
+  public decrement() {
+    this.count -= 1;
+  }
+
+  ngOnInit(): void {}
 }
