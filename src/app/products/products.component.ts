a849b5698c3de09433a90635dbe1e68e1cf27784
KilianOlry <kilian.olry@gmail.com>
2024-04-09 21:22:01 +0200

    feat(products.html / products.ts): add products

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,36 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrl: './products.component.css',
 })
-export class ProductsComponent {
-  products = [
-    {
-      name: 'MacBook',
-      price: 1550,
-      description: 'je suis un ordinateur',
-    },
-    {
-      name: 'MacBook',
-      price: 1550,
-      description: 'je suis un ordinateur',
-    },
-    {
-      name: 'MacBook',
-      price: 1550,
-      description: 'je suis un ordinateur',
-    },
-    {
-      name: 'MacBook',
-      price: 1550,
-      description: 'je suis un ordinateur',
-    },
-    {
-      name: 'MacBook',
-      price: 1550,
-      description: 'je suis un ordinateur',
-    },
-  ];
+export class ProductsComponent implements OnInit {
+  
+  public products : any;
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+    this.products = [
+      {
+        name: 'MacBook',
+        price: 1550,
+        description: 'je suis un ordinateur',
+      },
+      {
+        name: 'MacBook',
+        price: 1550,
+        description: 'je suis un ordinateur',
+      },
+      {
+        name: 'MacBook',
+        price: 1550,
+        description: 'je suis un ordinateur',
+      },
+      {
+        name: 'MacBook',
+        price: 1550,
+        description: 'je suis un ordinateur',
+      },
+      {
+        name: 'MacBook',
+        price: 1550,
+        description: 'je suis un ordinateur',
+      },
+    ]
+  }
 }
