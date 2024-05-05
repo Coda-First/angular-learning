34afcfdadd47459c529d89049211933365ddbbc3
Raxuis <raclot.raphael@gmail.com>
2024-05-05 12:48:49 +0200

    fix(categories): fix issue with names

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CategorysComponent } from './categories/categories.component';
+import { CategoriesComponent } from './categories/categories.component';
 import { ProductsComponent } from './products/products.component';
 
 const routes: Routes = [
   { path: 'products', component: ProductsComponent, title: 'Produits' },
-  { path: 'categorys', component: CategorysComponent, title: 'Categories' }
+  { path: 'categories', component: CategoriesComponent, title: 'Categories' }
 ];
 
 @NgModule({
